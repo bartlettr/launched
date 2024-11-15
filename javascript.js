0a0cf5f343b87a860c6fd0a5cc7bbbfa9a831b83
R Bartlett <bartlettr@users.noreply.github.com>
2024-11-15 08:39:26 -0500

    Update javascript.js
    added links

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 var links = [
   'https://www.youtube.com/watch?v=Awf45u6zrP0', //Cat jumps SAIL
   'https://www.youtube.com/watch?v=9vmQ6qPLm68', //blob launch compilation
+  'https://www.instagram.com/reel/C3kX-68IdIB/?igsh=amcycW80enN0bnp3', //wheelbarrow flips
   'https://www.youtube.com/watch?v=yypY-jjJmn4', //insane sledding jump in Wyoming
   'https://www.youtube.com/watch?v=KDy0WzKq-Cw', //Grandma milk tossing
   'https://www.youtube.com/watch?v=-u7hMRHkzlI', //kid ejected off tube behind surf boat
   'https://www.youtube.com/watch?v=NMpAE4L7n2Y', //catapult a Citroen
   'https://www.youtube.com/watch?v=RaFwyrN8M0U', //ship launch
+  'https://www.instagram.com/reel/C7xpk8oNx90/?igsh=MWhsNGdrd2JibWhm', //small child on trampoline
   'https://www.youtube.com/shorts/VzY3WQhJc5U', //trampoline kid
   'https://www.youtube.com/watch?v=4LMuk4gAkqc', //Super Dave touchdown to dollars
   'https://www.youtube.com/watch?v=BjKY9kYL57Y', //jet launch
+  'https://www.instagram.com/reel/DBNLrXAureY/?igsh=MXgwOWFuMGJ1bGxoOA%3D%3D', //dog jumps on guy off trampoline
   'https://www.youtube.com/shorts/VWFA2ZTB8kw', //people launching a guy on a trampoline
   'https://www.youtube.com/watch?v=hgeviZ7rfdQ', //kid (and boot) getting launched snow tubing
   'https://www.youtube.com/watch?v=cs8gamnMIS0', //medieval trebuchet
